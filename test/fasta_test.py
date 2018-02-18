0084f95cd1209dec715f35cfb7b3ec7a60a1d528
Hakan YILMAZ <hakanyilmaz42@gmail.com>
2018-02-18 12:37:53 +0300

    fasta dosyasını pars ederek sqllite dosyası oluşturan bölüm

--- test/fasta_test.py
+++ test/fasta_test.py
@@ -1,5 +1,5 @@
 from mkt.parser import Fasta
 
-f = Fasta.from_file(r'/home/mkturan/Repository/bioinformatics/test/PF00014.fasta')
+f = Fasta.from_file(r'd:/py3/bioinformatics/test/PF00014.fasta')
 for i in f:
     print(i, i.title)
